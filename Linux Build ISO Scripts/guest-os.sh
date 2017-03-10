31aef9e72e0313e7d086db557597bdad7b0d77cb
Darren Rainey <darren@darrenraineys.co.uk>
2017-03-10 20:33:08 +0000

    Create guest-os.sh

---
+++ Linux Build ISO Scripts/guest-os.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This file is used in the chroot (Guest OS) to setup the os and unmount/exit after complete
+
+apt-get install dialog dbus
+dbus-uuidgen > /var/lib/dbus/machine-id
+apt-get install linux-image-amd64 live-boot
+passwd
+
+
+
+
+echo "Exiting Chroot"
+apt-get clean
+rm /var/lib/dbus/machine-id && rm -rf /tmp/*
+unmount /proc /sys /dev/pts
+exit
