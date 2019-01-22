cc52bdd8d3670b6da692d9eb002929cf54a66c76
Darren Rainey <darren@darrenraineys.co.uk>
2019-01-22 16:38:24 +0000

    Update 10-minute-mail-net-api.sh

--- Tools/Misc/10-minute-mail-net-api.sh
+++ Tools/Misc/10-minute-mail-net-api.sh
@@ -1,8 +1,8 @@
 # GET A FRESH EMAIL ADDRESS AND SAVE THE COOKIE FOR LATER USE
-GETADDRESS=$(curl -q -# https://10minutemail.net/address.api.php -c cookie.jar| jq .)
+GETADDRESS=$(curl -s -q -# https://10minutemail.net/address.api.php -c cookie.jar| jq .)
 
 # GET ALL EMAILS FROM THAT ADDRESS
-GETMAIL=$(curl https://10minutemail.net/address.api.php -b cookie.jar | jq .)
+GETMAIL=$(curl -s -q -# https://10minutemail.net/address.api.php -b cookie.jar | jq .)
 
 # PRINT EMAILS IN JSON
 jq . "$GETMAIL"
